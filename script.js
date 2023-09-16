3415bf029915b7153335261ea1835cbd468d1baa
jackie-hen <144840436+jackie-hen@users.noreply.github.com>
2023-09-16 12:49:36 +0400

    Update script.js

--- script.js
+++ script.js
@@ -1,22 +1,19 @@
-var currentImage = document.getElementById("current-image");
+
 
 var imageThumbs = document.getElementById("image-thumbs");
+var currentImage = document.getElementById("current-image");
 
 for (var i = 1; i <= 8; i++) {
   var thumb = document.createElement("img");
+  thumb.src = "images/image" + i + ".jpg";
+  thumb.alt = "Image " + i;
+  thumb.classList.add("thumb");
+  imageThumbs.appendChild(thumb);
   thumb.addEventListener(
-  "click", function() {
-    currentImage.src = this.src;
-    currentImage.alt = this.alt;
-
-  }
-);
-  
-
+    "click", function() {
+      currentImage.src = this.src;
+    }
+  );
 }
-thumb.src = "img/image" + i + ".jpg";
-thumb.alt = "Image " + i;
-thumb.classList.add("thumb");
-imageThumbs.appendChild(thumb);
 
 
