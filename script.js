4f1cff59f3fd413a4255f39fa67c9d01e772c9b0
jackie-hen <144840436+jackie-hen@users.noreply.github.com>
2023-09-16 15:47:23 +0400

    Update script.js

--- script.js
+++ script.js
@@ -5,7 +5,7 @@ var currentImage = document.getElementById("current-image");
 
 for (var i = 1; i <= 8; i++) {
   var thumb = document.createElement("img");
-  thumb.src = "images/image" + i + ".jpg";
+  thumb.src = "img/image" + i + ".jpg";
   thumb.alt = "Image " + i;
   thumb.classList.add("thumb");
   imageThumbs.appendChild(thumb);
